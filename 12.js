69209f9062bb4a590b2c273ebcffa31a12d981d0
chandrika-kp <102351627+chandrika-kp@users.noreply.github.com>
2023-03-27 22:04:16 +0530

    Update 12.js

--- 12.js
+++ 12.js
@@ -1,17 +1,29 @@
-// Q) Remove except vibgyor ("v","i","b","g","y","o","r") letters in the given string
+// Q) print only vibgyor ("v","i","b","g","y","o","r") letters of the given string
 let str = 'catdogbat'
-const alphabet = ["v","i","b","g","y","o","r"];
-str=str.replace(/[acdefhjklmnpqstuwxz]/g, "");
-console.log(str)
+// const wantchars = ["v","i","b","g","y","o","r"];
+str=str.replace(/[acdefhjklmnpqstuwxz]/g, "");  // replace other characters as empty values   
+console.log(str)  // ogb
+
+or
+
+let filterstr = 'chandrikavibgyorpoorna'
+const wantedChars = ['a','c','d','e','f','h','j','k','l','m','n','p','q','s','t','u','w','x','z']
+let splitText = filterstr.split('') 
+let filterText = splitText.filter(i => wantedChars.includes(i))  // include(i) is nothing but indexOf(i) >= 0
+let finalTest =filterText.join('')
+console.log(finalTest) //chandkapna
+
+
+
 
 // Q) Remove vowels in the given string
 let str1 = 'catdogbat'
 const alphabets1 = ["a","e","i","o","u"];
 str1=str1.replace(/[aeiou]/g, "");
-console.log(str1)
+console.log(str1) // ctdgbt
 
 //  Q) Remove alphabets in the given string
 let str2 = '1ca2tdo3gbat'
 str2=str2.replace(/[a-z]/g, "");
-console.log(str2)
+console.log(str2)  // 123
   

2700da508aa75aa261bb6377fe4f21b243e9a390
chandrika-kp <102351627+chandrika-kp@users.noreply.github.com>
2023-11-09 10:56:19 +0530

    Update DisplayDateTime.js

--- DisplayDateTime.js
+++ DisplayDateTime.js
@@ -9,3 +9,6 @@ function App(props) {
 }
 
 export default App;
+
+# React-Project
+![2023-03-28 (1)](https://user-images.githubusercontent.com/102351627/228139998-a4c99c9c-6c04-495f-8057-620bdf6cf0a1.png)
